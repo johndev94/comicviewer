2b83d25047fa3860b595dbb318eaeee270e3c4f7
John Devaney <johndevaney1@hotmail.com>
2024-11-19 13:16:21 +0000

    First commit

---
+++ src/main/java/com/app/comicviewer/ComicviewerApplication.java
@@ -0,0 +1,13 @@
+package com.app.comicviewer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComicviewerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComicviewerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/app/comicviewer/ComicviewerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.comicviewer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComicviewerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
